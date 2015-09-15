51f678375ee3289850b33abd5356fab9e47637ee
Eric Marthinsen <eric@ericmarthinsen.com>
2015-09-15 00:56:19 -0400

    Prompt user for name, height, and width

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,16 +2,60 @@
 
 var generators = require('yeoman-generator');
 
-module.exports = generator.Base.extend({
+module.exports = generators.Base.extend({
   constructor: function() {
     generators.Base.apply(this, arguments);
   },
 
-  method1: function() {
-    console.log('method 1 just ran');
-  },
+  prompting: {
+    applicationName: function() {
+      var done = this.async();
 
-  method2: function() {
-    console.log('method 2 just ran');
+      this.prompt({
+          type:    'input',
+          name:    'name',
+          message: 'Application Name',
+          default: this.appname
+        },
+        function(answers) {
+          this.log(answers.name);
+          done();
+        }.bind(this)
+      );
+    },
+
+    height: function() {
+      var done = this.async();
+
+      this.prompt({
+          type:    'input',
+          name:    'height',
+          message: 'Screen Height',
+          default: 768,
+          store:   true
+        },
+        function(answers) {
+          this.log(answers.height);
+          done();
+        }.bind(this)
+                 );
+    },
+
+    width: function() {
+      var done = this.async();
+
+      this.prompt({
+          type:    'input',
+          name:    'width',
+          message: 'Screen Width',
+          default: 1024,
+          store:   true
+        },
+        function(answers) {
+          this.log(answers.width);
+          done();
+        }.bind(this)
+      );
+    }
   }
 });
